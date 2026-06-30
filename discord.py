a3d8c13696156915a439acfe11081b2ecf25abb3
u1429712535-png <u1429712535@gmail.com>
2026-06-30 17:37:40 +0200

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run("MTUxjNTYzODEwMzM3OTgwODI4Nw.GjdbX_.25wohQUD5FMpLEGNH5cMAlcSEHUExxaFtgxuLo4")
